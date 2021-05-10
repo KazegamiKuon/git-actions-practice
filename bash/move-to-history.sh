7c320d7bfdf60ed33212c1bcc4b95c0f98d97242
Kazegami <cuongquocdo1998@gmail.com>
2021-05-10 17:35:51 +0700

    update undo change

--- bash/move-to-history.sh
+++ bash/move-to-history.sh
@@ -5,16 +5,6 @@ git checkout $branch_name
 git reset HEAD~$backward_step
 read -n 1 -p "Will undo change? [Y/n]: " undo_change
 if [ "$undo_change" != "${undo_change#[Yy]}" ] ;then
-    git reset
-    git restore .
-    echo "All unstracked file would be clear"
-    git clean -n -dn
-    read -n 1 -p "Will clean all? [Y/n]: " clean_change
-    if [ "$clean_change" != "${clean_change#[Yy]}" ] ;then
-    # -f removing unstracked file
-    # -d include directories
-    # -x force removal of ignored files
-    git clean -xf -xd
-    fi
+    bash ./undo-change.sh
 fi
 echo -e "\nMove successfully!"
