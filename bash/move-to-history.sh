ffa297125088c6b57d32ddca8a2e21240c880376
Kazegami <cuongquocdo1998@gmail.com>
2021-05-10 17:55:30 +0700

    final 2

--- bash/move-to-history.sh
+++ bash/move-to-history.sh
@@ -5,18 +5,18 @@ git checkout $branch_name
 git reset HEAD~$backward_step
 read -n 1 -p "Will undo change? [Y/n]: " undo_change
 if [ "$undo_change" != "${undo_change#[Yy]}" ] ;then
-    # this script can see in undo-change.sh
-    git reset
-    git restore .
-    echo "All unstracked file would be clear"
-    git clean -n -dn
-    read -n 1 -p "Will clean all? [Y/n]: " clean_change
-    if [ "$clean_change" != "${clean_change#[Yy]}" ] ;then
-    # -f removing unstracked file
-    # -d include directories
-    # -x force removal of ignored files
-    git clean -xf -xd
-    fi
-    echo -e "\nUndo successful!"
+# this script can see in undo-change.sh
+git reset
+git restore .
+echo "All unstracked file would be clear"
+git clean -n -dn
+read -n 1 -p "Will clean all? [Y/n]: " clean_change
+if [ "$clean_change" != "${clean_change#[Yy]}" ] ;then
+# -f removing unstracked file
+# -d include directories
+# -x force removal of ignored files
+git clean -xf -xd
+fi
+echo -e "\nUndo successful!"
 fi
 echo -e "\nMove successfully!"
--- bash/undo-change.sh
+++ bash/undo-change.sh
@@ -4,6 +4,7 @@ echo "All unstracked file would be clear"
 git clean -n -dn
 read -n 1 -p "Will clean all? [Y/n]: " clean_change
 if [ "$clean_change" != "${clean_change#[Yy]}" ] ;then
+echo -e "\n"
 # -f removing unstracked file
 # -d include directories
 # -x force removal of ignored files
